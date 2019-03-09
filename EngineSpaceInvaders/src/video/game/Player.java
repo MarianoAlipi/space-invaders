d1af5099634461b0cc8cae4dbcbfec051cd4cb83
Mariano Alipi <marianoalipi@gmail.com>
2019-03-08 20:38:09 -0600

    Made the game runnable (althoug still bare-bones). Swapped images with the new ones.

---
+++ EngineSpaceInvaders/src/video/game/Player.java
@@ -0,0 +1,78 @@
+package video.game;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+/**
+ *
+ * @author MarcelA00821875
+ * @author MarianoA00822247
+ */
+public class Player extends Item {
+    
+    private int speed;                      // the player's speed
+    private Game game;                      // the running game
+    
+    /**
+     * Constructor for the player
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param game 
+     */
+    public Player(int x, int y, int width, int height, Game game){
+      //send x and y to the Item constructor
+      super(x, y);
+      this.width = width;
+      this.height = height;
+      this.game = game;
+      this.speed = 5;
+      this.hitbox = new Rectangle(x, y, width, height);
+    }
+    
+    /**
+     * Get speed
+     * @return speed
+     */
+    public int getSpeed() {
+        return speed;
+    }
+    
+    /**
+     * Set speed
+     * @param speed 
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    // tick() is used to tick the player
+    @Override
+    public void tick() {
+        // Move the player depending on flags
+        if (game.getKeyManager().left) {
+          setX(getX() - getSpeed());
+        }
+        if (game.getKeyManager().right) {
+          setX(getX() + getSpeed());
+        }
+      
+        // Reset x position and y position if collision with screen limits
+        if (getX() + getWidth() >= game.getWidth()){
+          setX(game.getWidth() - getWidth());
+        } else if (getX() <= 0) {
+          setX(0);
+        }
+        
+        // Relocate hitbox
+        hitbox.setLocation(getX(), getY());
+    }
+    
+    @Override
+    public void render(Graphics g){
+        g.drawImage(Assets.player, getX(), getY(), getWidth(), getHeight(), null);
+        // Draw the hitbox (for debugging)
+        // g.drawRect((int)getHitbox().getX(), (int)getHitbox().getY(), (int)getHitbox().getWidth(), (int)getHitbox().getHeight());
+    }
+
+}
