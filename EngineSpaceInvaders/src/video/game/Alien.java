84efb17c661c0776beb82f67e7ed1c2e3939d8df
Mariano Alipi <marianoalipi@gmail.com>
2019-03-08 18:17:37 -0600

    Converted Block to Alien. Modified Assets files.

---
+++ EngineSpaceInvaders/src/video/game/Alien.java
@@ -0,0 +1,63 @@
+package video.game;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ * @author MarcelA00821875
+ * @author MarianoA00822247
+ */
+public class Alien extends Item {
+    
+    private Game game;
+    private boolean visible;    // to show or hide the alien whether it's dead or not
+    private SoundClip explosion;    // sound effect for when the alien is killed
+    
+    /**
+     * Constructor for the blocks
+     * @param x
+     * @param y
+     * @param hits
+     * @param g 
+     */
+    public Alien(int x, int y, int hits, Game g) {
+        super(x, y);
+        this.width = 12;
+        this.height = 12;
+        this.game = g;
+        this.hitbox = new Rectangle(getX(), getY(), getWidth(), getHeight());
+        this.explosion = new SoundClip("/sounds/explosion.wav");
+        this.visible = true;
+    }
+
+    @Override
+    public void tick() {
+        // Check for collision with shot
+        if (getHitbox().intersects(game.getShot().getHitbox())) {
+            setVisible(false);
+            explosion.play();
+        }
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.alien, getX(), getY(), getWidth(), getHeight(), null);
+    }
+
+    /**
+     * Get visible
+     * @return visible
+     */
+    public boolean isVisible() {
+        return visible;
+    }
+
+    /**
+     * Set visible
+     * @param visible 
+     */
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+    
+}
--- EngineSpaceInvaders/src/video/game/Assets.java
+++ EngineSpaceInvaders/src/video/game/Assets.java
@@ -10,41 +10,19 @@ import java.awt.image.BufferedImage;
 public class Assets {
 
     public static BufferedImage background;
-    public static BufferedImage bar;
-    public static BufferedImage ball;
-    public static BufferedImage powerGood;
-    public static BufferedImage powerBad;
-    public static BufferedImage block;
-    public static BufferedImage policeSprite;
-    public static BufferedImage policeCarCollision[];
-    public static BufferedImage grenadeSprite;
-    public static BufferedImage grenadeRotate[];
-    public static BufferedImage meth[];
+    public static BufferedImage player;
+    public static BufferedImage alien;
+    public static BufferedImage shot;
+    public static BufferedImage bomb;
+    public static BufferedImage explosion;
             
     public static void init() {
         background = ImageLoader.loadImage("/images/background.png");
-        bar = ImageLoader.loadImage("/images/policeCar.png");
-        ball = ImageLoader.loadImage("/images/ball.png");
-        powerGood = ImageLoader.loadImage("/images/powerGood.png");
-        powerBad = ImageLoader.loadImage("/images/powerBad.png");
-        block = ImageLoader.loadImage("/images/meth.jpg");
-        policeSprite = ImageLoader.loadImage("/images/policeCarSprite.png");
-        SpriteSheet police = new SpriteSheet(policeSprite);
-        policeCarCollision = new BufferedImage[8];
-        grenadeSprite = ImageLoader.loadImage("/images/grenadeSprite.png");
-        SpriteSheet grenade = new SpriteSheet(grenadeSprite);
-        grenadeRotate = new BufferedImage[8];
-        for(int i = 0; i < 8; i++){
-            policeCarCollision[i] = police.crop(i * 352,0,352,134);
-        }
-        for(int i = 0; i < 8; i++){
-            grenadeRotate[i] = grenade.crop(i * 246,0,246,285);
-        }
-        
-        meth = new BufferedImage[3];
-        for (int i = 1; i <= 3; i++) {
-            meth[i - 1] = ImageLoader.loadImage("/images/meth" + i + ".png");
-        }
+        player = ImageLoader.loadImage("/images/player.png");
+        alien = ImageLoader.loadImage("/images/alien.png");
+        shot = ImageLoader.loadImage("/images/shot.png");
+        bomb = ImageLoader.loadImage("/images/bomb.png");
+        explosion = ImageLoader.loadImage("/images/explosion.png");
     }
 }
 
